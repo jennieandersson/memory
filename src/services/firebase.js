0ddd80e6f2293ff17d82cda38bdb92e32962576c
Jennie Andersson <jennie.andersson@vinnovera.se>
2023-01-18 11:27:44 +0100

    Update packages

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,18 +1,17 @@
 import firebase from 'firebase'
-import app from "firebase/app"
-import "firebase/firestore"
-import "firebase/auth"
+import app from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
 
 const config = {
-  apiKey: "AIzaSyCZizTJ-SFWG2WOdk6HnMKehrf6ilTJiDU",
-  authDomain: "memory-52471.firebaseapp.com",
-  projectId: "memory-52471",
-  storageBucket: "memory-52471.appspot.com",
-  messagingSenderId: "712411416174",
-  appId: "1:712411416174:web:c90b4c9205bddddba8487f",
-  measurementId: "G-3TGHN2QJL8"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 }
-
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config)
@@ -25,15 +24,12 @@ export const db = app.firestore()
 export const saveHighscore = (player, score) => {
   db.collection('highscoreList').doc().set({
     player: player,
-    score: score
+    score: score,
   })
 }
 
 export const getHighscore = () => {
-  return db.collection('highscoreList')
-    .orderBy('score', 'desc')
-    .limit(10)
-    .get()
+  return db.collection('highscoreList').orderBy('score', 'desc').limit(10).get()
 }
 
 /* export const streamHighscoreList = (observer) => {
